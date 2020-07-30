266b28d05bc88cbfad9bf9e4beb2bf551148676b
Chau Minh <hanhchaumn@gmail.com>
2020-07-30 19:27:46 +0700

    Paginate and sort search results

--- scripts/logsearch.js
+++ scripts/logsearch.js
@@ -1,3 +1,5 @@
+import normalize from '../libs/remove-whitespace.js';
+
 class SearchResult {
 	constructor({ message, prev, next }) {
 		this.message = message;
@@ -21,7 +23,26 @@ class SearchResult {
 	}
 }
 
+class Searcher {
+	constructor() {
+		this._div = document.createElement('div');
+	}
+
+	matches(message, searchTerm) {
+		const normalizedSearchTerm = normalize(searchTerm.trim().toLowerCase());
+		const content = message.data.content;
+		this._div.innerHTML = content;
+		const textContent = this._div.textContent || this._div.innerText || '';
+		const normalizedContent = normalize(textContent.trim().toLowerCase());
+		return normalizedContent.includes(normalizedSearchTerm);
+	}
+}
+
 export default class LogSearch extends Application {
+	CONFIG = {
+		SEARCH_BATCH_SIZE: 25
+	};
+
 	constructor(popout, ...args) {
 		super(...args);
 
@@ -42,6 +63,14 @@ export default class LogSearch extends Application {
 		this._element = undefined;
 
 		this._popout = popout;
+
+		this._page = 1;
+
+		this._totalPage = -1;
+
+		this._searcher = new Searcher();
+
+		this._mode = 'newest';
 	}
 
 	get collapsed() {
@@ -97,6 +126,21 @@ export default class LogSearch extends Application {
 		html.find('.close-button').on('click', () => {
 			this.clearSearch();
 		});
+
+		html.on('click', '.paginator.prev', () => {
+			this._page = Math.max(0, this._page - 1);
+			this.renderResultPage(this._page, this.CONFIG.SEARCH_BATCH_SIZE);
+		});
+
+		html.on('click', '.paginator.next', () => {
+			this._page = Math.min(this._totalPage, this._page + 1);
+			this.renderResultPage(this._page, this.CONFIG.SEARCH_BATCH_SIZE);
+		});
+
+		html.on('click', '.controls .sort button', (event) => {
+			const sortMode = $(event.target).attr('data-sort');
+			this.changeSortMode(sortMode);
+		});
 	}
 
 	hide() {
@@ -119,40 +163,128 @@ export default class LogSearch extends Application {
 		}
 	}
 
-	async searchInput(searchTerm) {
-		if (searchTerm && searchTerm != this._lastSearchTerm) {
-			const messages = game.messages.entities;
-			this._lastSearchTerm = searchTerm;
-			// this.results = messages.filter((m) => m.data.content.toLowerCase().includes(searchTerm));
-			this.results = [];
-			for (let i = 0; i < messages.length; i++) {
-				if (messages[i].data.content.toLowerCase().includes(searchTerm)) {
-					this.results.push(
-						new SearchResult({
-							message: messages[i],
-							prev: i > 0 ? messages[i - 1] : undefined,
-							next: i < messages.length - 1 ? messages[i + 1] : undefined
-						})
-					);
-				}
-			}
-			await this.renderSearchResults();
+	changeSortMode(newMode) {
+		if (newMode !== this._mode) {
+			this._mode = newMode;
+			this._page = 1;
+			this.results = this._sortResults(this._mode);
+			this.renderResultPage(this._page, this.CONFIG.SEARCH_BATCH_SIZE);
+			this._element.find('.sort button').removeClass('selected');
+			this._element.find(`.sort button[data-sort=${newMode}]`).addClass('selected');
 		}
 	}
 
-	async renderSearchResults() {
-		const searchResults = this._element.find('.results');
-		const html = [];
-		for (const result of this.results) {
-			const node = await this._renderOneResult(result);
-			html.push(node);
+	async searchInput(searchTerm) {
+		if (searchTerm) {
+			if (searchTerm != this._lastSearchTerm) {
+				const messages = game.messages.entities;
+				this._lastSearchTerm = searchTerm;
+				// this.results = messages.filter((m) => m.data.content.toLowerCase().includes(searchTerm));
+				this.results = [];
+				for (let i = 0; i < messages.length; i++) {
+					if (this._searcher.matches(messages[i], searchTerm)) {
+						this.results.push(
+							new SearchResult({
+								message: messages[i],
+								prev: i > 0 ? messages[i - 1] : undefined,
+								next: i < messages.length - 1 ? messages[i + 1] : undefined
+							})
+						);
+					}
+				}
+				this.results = this._sortResults(this._mode);
+				this._page = 1;
+				this._totalPage = Math.ceil(this.results.length / this.CONFIG.SEARCH_BATCH_SIZE);
+				await this.renderSearchResults(this._page, this.CONFIG.SEARCH_BATCH_SIZE);
+			} else if (this._page > 1) {
+				this._page = 1;
+				await this.renderSearchResults(this._page, this.CONFIG.SEARCH_BATCH_SIZE);
+			}
 		}
-		searchResults.html(html);
+	}
+
+	_sortResults(mode) {
+		if (mode === 'newest') {
+			return this.results.sort((a, b) => b.message.data.timestamp - a.message.data.timestamp);
+		} else if (mode === 'oldest') {
+			return this.results.sort((a, b) => a.message.data.timestamp - b.message.data.timestamp);
+		}
+		return this.results;
+	}
+
+	async renderSearchResults(page, pageSize) {
+		this.renderResultPage(page, pageSize);
 		if (this.results.length > 0) {
 			this._showCloseControl();
 		} else {
 			this._showSearchControl();
 		}
+		this.renderControls();
+	}
+
+	renderControls() {
+		const controls = this._element.find('.controls');
+		const resultCount = controls.find('.result-count');
+		const sort = controls.find('.sort');
+		const length = this.results.length;
+		controls.removeClass('none');
+		if (length > 0) {
+			controls.addClass('active');
+			let count = `${length} Results`;
+			if (length === 1) {
+				count = '1 Result';
+			}
+			resultCount.html(count);
+		} else {
+			controls.removeClass('active');
+			resultCount.html('No results.');
+		}
+	}
+
+	async _renderPagination() {
+		const pagination = $('<div class="pagination"></div>');
+		if (this._totalPage > 1) {
+			pagination.append($('<button class="paginator prev"><i class="fas fa-angle-left"></i></button>'));
+			pagination.append($(`<div class="page-info">Page ${this._page} of ${this._totalPage}</div>`));
+			pagination.append($('<button class="paginator next"><i class="fas fa-angle-right"></i></button>'));
+			pagination.removeClass('none');
+			this._updatePagination(pagination);
+		} else {
+			pagination.addClass('none');
+		}
+		return pagination;
+	}
+
+	async _updatePagination(pagination) {
+		const canGoBack = this._page > 1;
+		const canGoForward = this._page < this._totalPage;
+		if (!canGoBack) {
+			pagination.find('.paginator.prev').attr('disabled', true).addClass('disabled');
+		}
+		if (!canGoForward) {
+			pagination.find('.paginator.next').attr('disabled', true).addClass('disabled');
+		}
+	}
+
+	async renderResultPage(page, pageSize) {
+		const searchResults = this._element.find('.results');
+		const html = [];
+		const paginatedResults = this._paginateResults(page, pageSize);
+		for (const result of paginatedResults) {
+			const node = await this._renderOneResult(result);
+			html.push(node);
+		}
+		searchResults.html(html);
+		searchResults.append(await this._renderPagination());
+		setTimeout(() => {
+			searchResults.scrollTop(0);
+		}, 0);
+	}
+
+	_paginateResults(page, pageSize) {
+		const start = (page - 1) * pageSize;
+		const end = start + pageSize - 1;
+		return this.results.slice(start, end);
 	}
 
 	async _showSearchControl() {
@@ -169,8 +301,10 @@ export default class LogSearch extends Application {
 		this.results = [];
 		this._lastSearchTerm = undefined;
 		this._element.find('.results').empty();
+		this._element.find('.pagination').empty();
 		this._element.find('#search-input').val('');
 		this._showSearchControl();
+		this._element.find('.controls').addClass('none');
 	}
 
 	async _renderOneResult(result) {
@@ -232,7 +366,8 @@ export default class LogSearch extends Application {
 	async _scrollTo($chatLog, target) {
 		if (target.length) {
 			const msg = target[0];
-			const scrollTop = msg.offsetTop - $chatLog[0].clientHeight / 2 - msg.clientHeight;
+			// const scrollTop = msg.offsetTop - $chatLog[0].clientHeight / 2 - msg.clientHeight;
+			const scrollTop = msg.offsetTop - $chatLog[0].clientHeight / 2 + msg.clientHeight / 2;
 			$chatLog.scrollTop(scrollTop);
 		}
 	}
